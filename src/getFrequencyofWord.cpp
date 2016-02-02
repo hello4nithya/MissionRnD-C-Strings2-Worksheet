ff3a70b3c25264d35c956634c1eb91cbc4490e91
hello4nithya <hello4nithya@missionrnd.com>
2016-02-02 12:50:34 +0530

    Submit Commit

--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -11,9 +11,90 @@ Note: Dont modify original str or word,Just return count ,Spaces can also be par
 */
 
 #include <stdlib.h>
+int xstrlen(char *s){
 
-int count_word_in_str_way_1(char *str, char *word){
-	return 0;
+	char *p = s;
+
+	while (*p != '\0'){
+		p++;
+	}
+
+	return (p - s);
+
+}
+char *xstrcpy(char *target, const char *source)
+{
+	int i;
+
+	for (i = 0; source[i] != '\0'; ++i)
+		target[i] = source[i];
+	target[i] = source[i];
+
+	return target;
+}
+
+
+char *xstrstr(char *string, char *substring)
+{
+	 char *a, *b;
+
+	/* First scan quickly through the two strings looking for a
+	* single-character match.  When it's found, then compare the
+	* rest of the substring.
+	*/
+
+	b = substring;
+	if (*b == 0) {
+		return string;
+	}
+	for (; *string != 0; string += 1) {
+		if (*string != *b) {
+			continue;
+		}
+		a = string;
+		while (1) {
+			if (*b == 0) {
+				return string;
+			}
+			if (*a++ != *b++) {
+				break;
+			}
+		}
+		b = substring;
+	}
+	return (char *)0;
+}
+
+
+int count_word_in_str_way_1(char *string, char *word){
+	//char name[] = "ab cd ab cd";
+	//char r[] = "ab cd";
+	if (xstrlen(string) == 0 || xstrlen(word) == 0){
+		return 0;
+	}
+	if (xstrlen(word) > xstrlen(string)){
+		return 0;
+	}
+	int iLen = xstrlen(word);
+
+	int iCount = 0;
+
+
+
+	while (1)
+	{
+		if (xstrstr(string, word) == NULL)
+			break;
+		xstrcpy(string, xstrstr(string, word));
+
+		xstrcpy(string, string + iLen);
+		iCount++;
+	}
+
+	//printf("%d", iCount);
+	//getch();
+
+	return iCount;
 }
 
 int count_word_int_str_way_2_recursion(char *str, char *word){
--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -11,6 +11,66 @@ Note:Dont modify original string Neglect Spaces at the right end and at left end
 #include <stdlib.h>
 
 char * get_last_word(char * str){
+	int i, j, k = 0, m = 0, h = 0;
+	char *s = (char*)malloc(200 * sizeof(char));
+	//char *s1 = (char*)malloc(200 * sizeof(char));
+	if (str == NULL){
+		return NULL;
+	}
+	else if (str == "   "){
+		str = "";
+			return str;
+	}
+	else{
+		for (i = 0; str[i] != '\0'; i++){
+			if (str[i] == ' '){
+				h++;
+			}
+		}
+		if (h != 0){
+			for (i = 0; str[i] != '\0'; i++);
+			j = i - 1;
+			//printf("%d", i);
 
-	return NULL;
+			while (1){
+				if (str[j] == ' '){
+					j--;
+					h++;
+				}
+				else
+					break;
+			}
+			for (k = j; str[k] != ' '; k--);
+
+			for (i = k + 1; i <= j; i++){
+				s[m] = str[i];
+				m++;
+			}
+			
+			s[m] = '\0';
+			return s;
+		}
+
+		else
+			return str;
+	}
 }
+
+/*#include <stdlib.h>
+
+char * get_last_word(char * str){
+	char *LastWord = (char*)malloc(100);
+	int iterW = 0;
+	for (int iter = 0; str[iter] != '\0'; iter++){
+		if (str[iter] != ' '){
+			//forms the last word
+			LastWord[iterW++] = str[iter];
+		}
+		else if (iterW != 0){
+			//forms the new word
+			iterW = 0;
+		}
+	}
+	LastWord[iterW] = '\0';
+	return LastWord;
+}*/
